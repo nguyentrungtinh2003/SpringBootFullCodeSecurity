c6dee45df0fb741bcfc88da9d75917fffc70e476
nguyentrungtinh <trungtinhn300@gmail.com>
2024-08-10 14:15:23 +0700

    Create Project Spring Boot Full Code Security

---
+++ src/main/java/com/TrungTinhFullStack/SpringBootFullCodeSecurity/SpringBootFullCodeSecurityApplication.java
@@ -0,0 +1,13 @@
+package com.TrungTinhFullStack.SpringBootFullCodeSecurity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootFullCodeSecurityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootFullCodeSecurityApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/TrungTinhFullStack/SpringBootFullCodeSecurity/SpringBootFullCodeSecurityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.TrungTinhFullStack.SpringBootFullCodeSecurity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootFullCodeSecurityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
